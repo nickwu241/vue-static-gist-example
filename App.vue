899b758268cf7fbdcd1de6a8268413d3cb73b076
nickwu241 <nick.wu@shopify.com>
2018-07-06 20:32:04 -0400

    Create example of using Gist.vue

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Example of a statically embedded Gist</h1>
+    <!-- Use the Component -->
+    <Gist />
+  </div>
+</template>
+
+<script>
+import Gist from './Gist.vue' // Import the Component
+
+export default {
+  name: 'App',
+  components: { Gist } // Register the Component
+}
+</script>
+
+<style>
+body {
+  max-width: 640px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+</style>
